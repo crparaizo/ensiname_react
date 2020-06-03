5f7883d7c76c5cc46ef6bb3a3bc0842f43fb9aee
crparaizo <candidaparaizo@gmail.com>
2020-06-03 01:02:04 -0300

    Implementação do layout

--- src/pages/landing/landingPage.js
+++ src/pages/landing/landingPage.js
@@ -7,115 +7,129 @@ import React from 'react';
 function Main() {
     return (
         <div>
-            <Menu />
-            <div className="div1">
-                <h1 className="font36">Ensina-Me</h1>
-                <p>Aulas online com professores especializados para o ensino fundamental</p>
+            <div className="banner">
+                <Menu />
             </div>
             <div className="textCenter">
                 <p className="font36">A melhor plataforma para aulas particulares online</p>
-                <p>Faça aulas particulares com os melhores professores de todo o Brasil, diretamente de sua casa.</p>
-                <p>Oferecemos uma solução de fácil usabilidade, repleta de recursos e com a mais alta qualidade.</p>
+                <p className="font22">Faça aulas particulares com os melhores professores de todo o Brasil, diretamente de sua casa.</p>
+                <p className="font22">Oferecemos uma solução de fácil usabilidade, repleta de recursos e com a mais alta qualidade.</p>
             </div>
             <div className="vantagens displayFlex">
-                <div className="conteudo_vantagem textCenter">
-                    <img src={require("../../img/logo.svg")} alt="logotipo" height="64px" />
+                <div className="conteudo_vantagem textCenter roxo">
+                    <img src={require("../../img/educacao.png")} height="64px" />
                     <h3>PRATICIDADE</h3>
                     <p>Aprenda direto de sua casa e no melhor horário para você.</p>
                 </div>
-                <div className="conteudo_vantagem textCenter">
-                    <img src={require("../../img/logo.svg")} alt="logotipo" height="64px" />
+                <div className="conteudo_vantagem textCenter amarelo">
+                    <img src={require("../../img/educacao.png")} height="64px" />
                     <h3>QUALIDADE</h3>
                     <p>Estude com os melhores professores de todo o Brasil.</p>
                 </div>
-                <div className="conteudo_vantagem textCenter">
-                    <img src={require("../../img/logo.svg")} alt="logotipo" height="64px" />
+                <div className="conteudo_vantagem textCenter cinza">
+                    <img src={require("../../img/educacao.png")} height="64px" />
                     <h3>ECONOMIA</h3>
                     <p>Planos personalizados e com aulas que cabem no seu bolso.</p>
                 </div>
+                <img className="img_vantagens" src={require("../../img/lapis.png")} />
             </div>
-            <div className="textCenter">
-                <p>A melhor qualidade de ensino, com a atenção e o acompanhamento que seu filho merece</p>
+            <div className="amarelo textCenter div4 displayFlex">
+                <p className="textTransicao">A melhor qualidade de ensino, com a atenção e o acompanhamento que seu filho merece</p>
             </div>
-            <div className="planos textCenter">
-                <h2 className="font36">Um plano para a sua realidade</h2>
-                <p>Temos três opções de planos para que você escolha a que melhor se encaixe a suas necessidades!</p>
+            <div className="planos textCenter displayFlex">
+                <h2 className="font36">Conheça Nossos Planos</h2>
+                <p className="font22">Temos três opções de planos para que você escolha a que melhor se encaixe a suas necessidades!</p>
                 <div className="div_planos displayFlex">
-                    <div className="plano_card">
-                        <h3>PLANO 1</h3>
-                        <p>Nulla non libero
-                        dapibus leo et, sagittis
-                        Proin porta libero vel
-                        eget lobortis mauris
-                        Nam eu luctus ante. In
-                        ullamcorper in velit vitae
-                     </p>
-                        <button>SAIBA MAIS</button>
+                    <div className="plano_card displayFlex">
+                        <div className="roxo_escuro">
+                            <h3 className="titulo_plano">PLANO BÁSICO</h3>
+                            <p className="preco_plano">R$ 399,00</p>
+                        </div>
+                        <ul className="lista_plano">
+                            <li>Plano de aulas</li>
+                            <li>Aulas exclusivas</li>
+                            <li>Plano Pedagogico</li>
+                            <li>4 Aulas</li>
+                            <li className="cinzaClaro">Atividades Extras</li>
+                            <li className="cinzaClaro">Plantão tira dúvidas</li>
+                        </ul>
+                        <button className="amarelo branco">SAIBA MAIS</button>
                     </div>
-                    <div className="plano_card">
-                        <h3>PLANO 2</h3>
-                        <p>Nulla non libero
-                        dapibus leo et, sagittis
-                        Proin porta libero vel
-                        eget lobortis mauris
-                        Nam eu luctus ante. In
-                    ullamcorper in velit vitae  </p>
-                        <button>SAIBA MAIS</button>
+                    <div className="plano_card displayFlex">
+                        <div className="roxo_escuro">
+                            <h3 className="titulo_plano">PLANO MÉDIO</h3>
+                            <p className="preco_plano"> R$ 769,00</p>
+                        </div>
+                        <ul className="lista_plano">
+                            <li>Plano de aulas</li>
+                            <li>Aulas exclusivas</li>
+                            <li>Plano Pedagogico</li>
+                            <li>8 Aulas</li>
+                            <li className="cinzaClaro">Atividades Extras</li>
+                            <li className="cinzaClaro">Plantão tira dúvidas</li>
+                        </ul>
+                        <button className="amarelo branco">SAIBA MAIS</button>
                     </div>
-                    <div className="plano_card">
-                        <h3>PLANO 3</h3>
-                        <p>Nulla non libero
-                        dapibus leo et, sagittis
-                        Proin porta libero vel
-                        eget lobortis mauris
-                        Nam eu luctus ante. In
-                    ullamcorper in velit vitae  </p>
-                        <button>SAIBA MAIS</button>
+                    <div className="plano_card displayFlex">
+                        <div className="roxo_escuro">
+                            <h3 className="titulo_plano">PLANO MASTER</h3>
+                            <p className="preco_plano">R$ 1.149,00</p>
+                        </div>
+                        <ul className="lista_plano">
+                            <li>Plano de aulas</li>
+                            <li>Aulas exclusivas</li>
+                            <li>Plano Pedagogico</li>
+                            <li>12 Aulas</li>
+                            <li>Atividades Extras</li>
+                            <li >Plantão tira dúvidas</li>
+                        </ul>
+                        <button className="amarelo branco">SAIBA MAIS</button>
                     </div>
                 </div>
             </div>
             <div className="professor displayFlex">
-                <h2 className="titulo_professor textCenter displayFlex">ÁREA DO PROFESSOR</h2>
                 <div className="conteudo_professor displayFlex">
-                    <div className="img_professor">
-                        <img src={require("../../img/logo.svg")} alt="logotipo" height="500px" />
-                    </div>
-                    <div className="texto_professor textCenter">
-                        <h3>SEJA PROTAGONISTA DA SUA PRÓPRIA CARREIRA</h3>
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <h3>AQUI VOCÊ TERÁ TODO O APOIO QUE PRECISA!</h3>
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <button>QUERO FAZER PARTE DO ENSINA-ME</button>
+                    <div className="texto_professor branco textCenter">
+                        <h2 className="titulo_professor branco font36 displayFlex">Professor</h2>
+                        <h3 className="font22">SEJA PROTAGONISTA DA SUA PRÓPRIA CARREIRA</h3>
+                        <p> Já pensou em  focar na sua paixão por ensinar, tomando suas próprias decisões? Se cadastrando em nossa plataforma, você terá flexibilidade e poderá trabalhar de onde e quando quiser.</p>
+                        <p> Conquiste mais alunos! Receba feedbacks, fique nas melhores posições de nosso ranking, ganhe visibilidade e ofereça as suas aulas para alunos em todo o Brasil.</p>
+                        <button className="amarelo branco">SAIBA MAIS</button>
                     </div>
                 </div>
             </div>
             <div className="contato displayFlex">
-                <div className="fale_conosco">
-                    <h2>Fale Conosco</h2>
-                    <p>Temos um especialista sempre pronto para atendê-los.</p>
-                    <div className="fale_item">
-                        <img src={require("../../img/logo.svg")} alt="logotipo" height="32px" />
-                        <p>Endereço</p>
+                <div className="fale_conosco textCenter">
+                    <h2 className="font36">Fale Conosco</h2>
+                    <p className="font22">Temos um especialista sempre pronto para atendê-los.</p>
+                    <div className="fale_item displayFlex">
+                        <img src={require("../../img/endereco.png")} height="32px" />
+                        <p>Avenida Paulista, 456 | Sala 82</p>
                     </div>
-                    <div className="fale_item">
-                        <img src={require("../../img/logo.svg")} alt="logotipo" height="32px" />
-                        <p>E-mail</p>
+                    <div className="fale_item displayFlex">
+                        <img src={require("../../img/email.png")} height="32px" />
+                        <p>contato@ensiname.com.br</p>
                     </div>
-                    <div className="fale_item">
-                        <img src={require("../../img/logo.svg")} alt="logotipo" height="32px" />
-                        <p>Telefone</p>
+                    <div className="fale_item displayFlex">
+                        <img src={require("../../img/telefone.png")} height="32px" />
+                        <p>(11) 3625-3625</p>
+                    </div>
+                    {/* Falta mudar esse icone: */}
+                    <div className="fale_item displayFlex">
+                        <img src={require("../../img/telefone.png")} height="32px" />
+                        <p>www.ensiname.com.br</p>
                     </div>
                 </div>
-                <div className="mande_duvidas">
-                    <h2>Mande sua Dúvida</h2>
-                    <p>Ficou com alguma dúvida?</p>
-                    <p>Estamos à sua disposição!</p>
+                <div className="mande_duvidas displayFlex">
+                    <h2  className="font36">Mande sua Dúvida</h2>
+                    <p  className="font22">Ficou com alguma dúvida?</p>
+                    <p  className="font22">Estamos à sua disposição!</p>
 
                     <form className="form_contato displayFlex">
-                        <input type="text" placeholder="NOME" />
-                        <input type="text" placeholder="EMAIL" />
-                        <input type="text" placeholder="MENSAGEM" />
-                        <input type="submmit" value="ENVIAR" />
+                        <input className="item_form" type="text" placeholder="NOME" />
+                        <input className="item_form" type="text" placeholder="EMAIL" />
+                        <input className="item_form" type="text" placeholder="MENSAGEM" />
+                        <input className="btn_form roxo_escuro" type="submmit" value="ENVIAR" />
                     </form>
                 </div>
             </div>
